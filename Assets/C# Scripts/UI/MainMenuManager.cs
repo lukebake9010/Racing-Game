d87d484d32a3976a6521f6c993e2d8048515a7c0
Blaine Marsh <imperialblaine@gmail.com>
2025-07-08 18:53:29 +0100

    Tweaked File structure
    - Tweaked File structure
    - Changed Main Menu script controller object
    - Added menu wrappers
    - Added button logic for moving between menu states using the wrappers

--- Assets/C# Scripts/UI/MainMenuManager.cs
+++ Assets/C# Scripts/UI/MainMenuManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class MainMenuManager : MonoBehaviour
 {
+    #region Variables
+
     /// <summary>
     /// The loading menu to activate on play
     /// </summary>
@@ -13,11 +15,40 @@ public class MainMenuManager : MonoBehaviour
     private LoadingMenu sceneLoadingMenu;
 
     /// <summary>
+    /// An internal reference to the transform the menu buttons are children to.
+    /// Synonymous with the menu manager, or the parent object of this script.
+    /// </summary>
+    private Transform _mainMenuWrapper;
+    private Transform _settingsMenuWrapper;
+    private Transform _creditsWrapper;
+
+    #endregion
+
+    #region Logic
+
+    private void Awake()
+    {
+        _mainMenuWrapper = transform.GetChild(0);
+        _settingsMenuWrapper = transform.GetChild(1);
+        _creditsWrapper = transform.GetChild(2);
+    }
+
+    private void Start()
+    {
+        foreach(Transform child in transform)
+            child.gameObject.SetActive(false);
+        _mainMenuWrapper.gameObject.SetActive(true);
+    }
+
+
+    /// <summary>
     /// Function for Logic for the "Play" button.
     /// </summary>
     public void OnPlayButton()
     {
-        if(sceneLoadingMenu != null)
+        Debug.Log("Play button pressed.");
+
+        if (sceneLoadingMenu != null)
         {
             int testSceneID = Utilities.SceneManagement.SceneUtils.GetSceneBuildIndexByName("TestScene");
             if (testSceneID != -1)
@@ -33,7 +64,21 @@ public class MainMenuManager : MonoBehaviour
     /// </summary>
     public void OnHighscoresButton()
     {
+        Debug.Log("Highscores button pressed.");
+    }
 
+    /// <summary>
+    /// Function for Logic for the "Settings" button.
+    /// </summary>
+    public void OnSettingsButton()
+    {
+        Debug.Log("Settings button pressed.");
+
+        //Disable the main menu
+        _mainMenuWrapper.gameObject.SetActive(false);
+      
+        //Open the settings menu
+        _settingsMenuWrapper.gameObject.SetActive(true);
     }
 
     /// <summary>
@@ -41,7 +86,13 @@ public class MainMenuManager : MonoBehaviour
     /// </summary>
     public void OnCreditsButton()
     {
+        Debug.Log("Credit button pressed.");
 
+        //Disable the main menu
+        _mainMenuWrapper.gameObject.SetActive(false);
+
+        //Open the settings menu
+        _creditsWrapper.gameObject.SetActive(true);
     }
 
     /// <summary>
@@ -53,7 +104,21 @@ public class MainMenuManager : MonoBehaviour
     /// </summary>
     public void OnQuitButton()
     {
+        Debug.Log("Quit button pressed.");
         //Quits the application
         Application.Quit();
     }
+
+    public void ReturnToMenu()
+    {
+        foreach (Transform child in transform)
+            child.gameObject.SetActive(false);
+        _mainMenuWrapper.gameObject.SetActive(true);
+    }
+
+    #endregion
+
+    #region Methods
+
+    #endregion
 }
