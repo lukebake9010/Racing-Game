93c6d9785b054b847f4f4f9d1ac9abca86bb827a
Blaine Marsh <imperialblaine@gmail.com>
2025-07-09 15:20:40 +0100

    Changed Structure of Main Menu
    Extracted Window Manager to inherited utility class.
    Changed reference to scene loader to a reference to the prefab to instantiate on awake function.

--- Assets/C# Scripts/UI/MainMenuManager.cs
+++ Assets/C# Scripts/UI/MainMenuManager.cs
@@ -4,121 +4,56 @@ using UnityEngine.SceneManagement;
 /// <summary>
 /// Basic Manager class containing functions for the main menu buttons.
 /// </summary>
-public class MainMenuManager : MonoBehaviour
+public class MainMenuManager : WindowedMenuManager
 {
     #region Variables
 
     /// <summary>
-    /// The loading menu to activate on play
+    /// A reference to the prefab of the loading menu.
+    /// Instantiated on Awake, and activated on play.
+    /// Keeps the object hierarchy "tidy".
     /// </summary>
     [SerializeField]
-    private LoadingMenu sceneLoadingMenu;
+    private GameObject loadingMenuPrefab;
 
     /// <summary>
-    /// An internal reference to the transform the menu buttons are children to.
-    /// Synonymous with the menu manager, or the parent object of this script.
+    /// A reference to the Instantiated loading menu to activate on play.
     /// </summary>
-    private Transform _mainMenuWrapper;
-    private Transform _settingsMenuWrapper;
-    private Transform _creditsWrapper;
+    private LoadingMenu _sceneLoadingMenu;
 
     #endregion
 
-    #region Logic
-
+    /// <summary>
+    /// Instantiate the prefab on awake and store the reference in a private variable.
+    /// </summary>
     private void Awake()
     {
-        _mainMenuWrapper = transform.GetChild(0);
-        _settingsMenuWrapper = transform.GetChild(1);
-        _creditsWrapper = transform.GetChild(2);
+        if(loadingMenuPrefab != null)
+            _sceneLoadingMenu = Instantiate(loadingMenuPrefab, parent:transform.parent).GetComponent<LoadingMenu>();
     }
 
-    private void Start()
-    {
-        foreach(Transform child in transform)
-            child.gameObject.SetActive(false);
-        _mainMenuWrapper.gameObject.SetActive(true);
-    }
-
+    #region Logic
 
     /// <summary>
     /// Function for Logic for the "Play" button.
     /// </summary>
     public void OnPlayButton()
     {
+        //DEBUG
         Debug.Log("Play button pressed.");
 
-        if (sceneLoadingMenu != null)
+        //If the scene loader exists, use it to switch scenes.
+        if (_sceneLoadingMenu != null)
         {
             int testSceneID = Utilities.SceneManagement.SceneUtils.GetSceneBuildIndexByName("TestScene");
             if (testSceneID != -1)
             {
-                sceneLoadingMenu.ShowAndLoadScene(testSceneID);//Show loading menu and load testscene
+                _sceneLoadingMenu.ShowAndLoadScene(testSceneID);//Show loading menu and load testscene
             }
             
         }
     }
 
-    /// <summary>
-    /// Function for Logic for the "Highscores" button.
-    /// </summary>
-    public void OnHighscoresButton()
-    {
-        Debug.Log("Highscores button pressed.");
-    }
-
-    /// <summary>
-    /// Function for Logic for the "Settings" button.
-    /// </summary>
-    public void OnSettingsButton()
-    {
-        Debug.Log("Settings button pressed.");
-
-        //Disable the main menu
-        _mainMenuWrapper.gameObject.SetActive(false);
-      
-        //Open the settings menu
-        _settingsMenuWrapper.gameObject.SetActive(true);
-    }
-
-    /// <summary>
-    /// Function for Logic for the "Credits" button.
-    /// </summary>
-    public void OnCreditsButton()
-    {
-        Debug.Log("Credit button pressed.");
-
-        //Disable the main menu
-        _mainMenuWrapper.gameObject.SetActive(false);
-
-        //Open the settings menu
-        _creditsWrapper.gameObject.SetActive(true);
-    }
-
-    /// <summary>
-    /// Function for Logic for the "Quit" button.
-    /// 
-    /// <para>
-    /// Simply exits the application.
-    /// </para>
-    /// </summary>
-    public void OnQuitButton()
-    {
-        Debug.Log("Quit button pressed.");
-        //Quits the application
-        Application.Quit();
-    }
-
-    public void ReturnToMenu()
-    {
-        foreach (Transform child in transform)
-            child.gameObject.SetActive(false);
-        _mainMenuWrapper.gameObject.SetActive(true);
-    }
-
     #endregion
 
-    #region Methods
-
-    #endregion
 }
